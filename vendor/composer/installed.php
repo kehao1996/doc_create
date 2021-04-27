6772b9628b1b4599c955dae70122d4f7f85d8488
kehao <46619057@qq.com>
2021-04-27 11:52:46 +0800

    更新以来

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '5bf16de99c68cdb0a43d242e04f549d49cd0a1fb',
+    'name' => 'kehao/doc_create',
+  ),
+  'versions' => 
+  array (
+    'kehao/doc_create' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5bf16de99c68cdb0a43d242e04f549d49cd0a1fb',
+    ),
+  ),
+);
