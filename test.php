ca73d7431ec705e9e576f90718849d802bde8c35
kehao <46619057@qq.com>
2021-04-27 15:47:17 +0800

    'gx'

--- test.php
+++ test.php
@@ -3,7 +3,7 @@ require_once 'vendor/autoload.php';
 
 $target_dir = 'F:\code\test';
 $con = 'F:\code\test';
-$con = 'F:\code\laravel\kehao\app\Http\Controllers';
+$con = 'F:\code\test';
 try {
     $doc = new \Doc\doc('test',$target_dir,$con,1);
     $base64 = $doc->init();
