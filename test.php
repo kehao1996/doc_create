25aed9a037f58c9f264a3ffd527eb81cbfcf8189
kehao <46619057@qq.com>
2021-04-27 11:38:49 +0800

    'gx'

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'vendor/autoload.php';
+
+$target_dir = 'F:\code\test';
+$con = 'F:\code\test';
+try {
+    $doc = new \Doc\doc('test',$target_dir,$con,1);
+    $base64 = $doc->init();
+    var_dump($base64);exit;
+}catch (\Doc\DocException $e){
+    var_dump($e->getMessage());
+}
+
